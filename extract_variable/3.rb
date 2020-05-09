765449487f9278dafd60b946e251b6d0bf3668d9
Anna <anna.voitik@gmail.com>
2020-05-09 14:59:20 +0100

    Extract variable task 3 done

--- extract_variable/3.rb
+++ extract_variable/3.rb
@@ -1,5 +1,7 @@
 def send_correct_page(browser, window)
-  if(browser.type == "Safari" && window.size < "768px")
+  active_browser = browser.type
+  size_window = window.size
+  if active_browser == "Safari" && size_window < "768px")
     return "You are using the Safari browser in a small window."
   else
     return "You are not using the Safari browser or have a big window."
